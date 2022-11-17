8a2a40a7888f7164628b361654cc35f665a6e7cc
Felipe Herrera <felipeth4@hotmail.com>
2022-11-17 05:06:31 -0500

    receive two url params to search the  nodes common ancestors

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ def hello_seti():
     return "Hello SETI, I'm Juan Felipe Tamayo and I really enjoyed this technical test"
 
 
-@app.route('/getCommonAncestor')
-def get_common_ancestor():
+@app.route('/getCommonAncestor/<int:node_one>/<int:node_two>')
+def get_common_ancestor(node_one, node_two):
     root = create_tree(70, 49, 37, 54, 84, 78, 85, 22, 40, 51, 76, 80)
-    return jsonify(common_node(root, 76, 85))
+    return jsonify(common_node(root, node_one, node_two))
 
 
 @app.route('/getNode/<int:data>')
