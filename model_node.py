c31f5004b9ce47d85547ed60786e35a45bf91c47
Felipe Herrera <felipeth4@hotmail.com>
2022-11-17 04:34:15 -0500

    functions created to find nodes in a binary tree

--- model_node.py
+++ model_node.py
@@ -18,3 +18,44 @@ class Node:
                     root.left = node
                 else:
                     self.add_node(root.left, node)
+
+
+def search_node(root, data):
+    if root is None:
+        message = "The node"+" "+str(data)+" "+"you searched doesn't exits"
+    elif root.data == data:
+        message = "The node"+" "+str(data)+" "+"you searched currently exits"
+    elif data < root.data:
+        message = search_node(root.left, data)
+    elif data > root.data:
+        message = search_node(root.right, data)
+
+    return message
+
+
+def search_common_node(root, x, y):
+    if root is None:
+        return None
+
+    if root.data > max(x, y):
+        return search_common_node(root.left, x, y)
+
+    elif root.data < min(x, y):
+        return search_common_node(root.right, x, y)
+
+    return root.data
+
+
+def common_node(root, x, y):
+    if not root or not search_node(root, x) or not search_node(root, y):
+        response = {"message": "the common node does not exist..."}
+        return response
+
+    common_search = search_common_node(root, x, y)
+
+    if common_search:
+        response = {"The common node is:": common_search}
+        return response
+    else:
+        response = {"message": "the common node does not exist..."}
+        return response
