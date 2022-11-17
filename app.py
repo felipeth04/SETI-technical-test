9a2bbd8221522c7116dc4edec2712251138bc4b2
Felipe Herrera <felipeth4@hotmail.com>
2022-11-17 04:38:37 -0500

    created the path to get a common ancestor in a binary tree

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify
+from flask import Flask, jsonify
 from model_node import *
 
 app = Flask(__name__)
@@ -8,6 +8,11 @@ app = Flask(__name__)
 def hello_seti():
     return "Hello SETI, I'm Juan Felipe Tamayo and I really enjoyed this technical test"
 
+
+@app.route('/getCommonAncestor')
+def get_common_ancestor():
+    root = create_tree(70, 49, 37, 54, 84, 78, 85, 22, 40, 51, 76, 80)
+    return jsonify(common_node(root, 76, 85))
 
 
 if __name__ == '__main__':
